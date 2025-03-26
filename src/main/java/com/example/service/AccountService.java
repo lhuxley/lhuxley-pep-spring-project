ca289b6856495a003bbfcaa5f0332700f2be22cc
Logan Helstad <loganhelstad@gmail.com>
2025-03-26 17:25:02 +0000

    readded some imports because removing them breaks some tests

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -3,6 +3,8 @@ package com.example.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.example.entity.Account;
@@ -10,7 +12,7 @@ import com.example.repository.AccountRepository;
 
 @Service
 public class AccountService {
-
+    
     private final AccountRepository accountRepository;
 
     @Autowired
@@ -21,8 +23,8 @@ public class AccountService {
     @Transactional
     public Account createAccount(Account account) {
         if (accountRepository.findByUsername(account.getUsername()).isPresent() ||
-                account.getUsername().length() < 4) {
-            return null;
+            account.getUsername().length() < 4) {
+            return null; 
         }
         return accountRepository.save(account);
     }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -2,10 +2,11 @@ package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
 import com.example.entity.Message;
 import java.util.Optional;
 import com.example.repository.MessageRepository;
